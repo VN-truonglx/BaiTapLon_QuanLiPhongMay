93dad5f7c105cd2466059419821fbd5d6617f015
ikira2014 <62959654+ikira2014@users.noreply.github.com>
2020-04-06 22:28:01 +0700

    frmMain design -commit

---
+++ QuanLyPhongMay_frmMain/ChildForms/QuanLyLichThucHanh/frmLichThucHanh.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLyPhongMay.ChildForms.QuanLyLichThucHanh
+{
+    public partial class frmLichThucHanh : Form
+    {
+        public frmLichThucHanh()
+        {
+            InitializeComponent();
+        }
+
+        private void LichThucHanh_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ QuanLyPhongMay_frmMain/QLPM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSancode
+{
+    public partial class QLPM : Form
+    {
+        public QLPM()
+        {
+            InitializeComponent();
+        }
+
+        private void lịchThựcHànhToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            QuanLyPhongMay.ChildForms.QuanLyLichThucHanh.frmLichThucHanh f = new QuanLyPhongMay.ChildForms.QuanLyLichThucHanh.frmLichThucHanh();
+            f.StartPosition = FormStartPosition.CenterScreen;
+            f.Show();
+
+        }
+
+        private void QLPM_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
